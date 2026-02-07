96dde227419969aa056fd1029661885f009ddb4c
vish03 <112392028+Vishwesh2303@users.noreply.github.com>
2026-02-08 00:13:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
   base: "/darshanandvidhi/",
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   server: {
     host: "::",
     port: 8080,
@@ -16,10 +18,11 @@ export default defineConfig({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
